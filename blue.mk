253149061a5cebbd3aec354bd6a98e39534263b0
freexperia <jerpelea@gmail.com>
2012-09-24 16:36:32 +0300

    blue: remove old charging animation

--- blue.mk
+++ blue.mk
@@ -29,16 +29,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
    device/sony/blue-common/prebuilt/logo_X.rle:root/logo.rle
 
-#Offline charging animation
-PRODUCT_COPY_FILES += \
-    device/sony/blue-common/prebuilt/animations/charging_animation_01_X.png:system/semc/chargemon/data/charging_animation_01.png \
-    device/sony/blue-common/prebuilt/animations/charging_animation_02_X.png:system/semc/chargemon/data/charging_animation_02.png \
-    device/sony/blue-common/prebuilt/animations/charging_animation_03_X.png:system/semc/chargemon/data/charging_animation_03.png \
-    device/sony/blue-common/prebuilt/animations/charging_animation_04_X.png:system/semc/chargemon/data/charging_animation_04.png \
-    device/sony/blue-common/prebuilt/animations/charging_animation_05_X.png:system/semc/chargemon/data/charging_animation_05.png \
-    device/sony/blue-common/prebuilt/animations/charging_animation_06_X.png:system/semc/chargemon/data/charging_animation_06.png \
-    device/sony/blue-common/prebuilt/animations/charging_animation_07_X.png:system/semc/chargemon/data/charging_animation_07.png
-
 # EGL config
 PRODUCT_COPY_FILES += \
     device/sony/blue-common/config/egl.cfg:system/lib/egl/egl.cfg
