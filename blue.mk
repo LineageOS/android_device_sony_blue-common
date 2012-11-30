35a6eb1352c8ce1fd509ee48ca8c6370fe50d05c
codeworkx <codeworkx@cyanogenmod.org>
2012-11-30 17:58:04 +0100

    blue-common: build tinymix, tweak speaker and headset volumes
    Change-Id: I615412c3b84454a73b5999cef1732d80b6c97c28

--- blue.mk
+++ blue.mk
@@ -85,7 +85,8 @@ PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio.usb.default \
     audio.r_submix.default \
-    libaudio-resampler
+    libaudio-resampler \
+    tinymix
 
 # BT
 PRODUCT_PACKAGES += \
