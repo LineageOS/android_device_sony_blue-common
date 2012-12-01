9b13f3d4e1a7aec9c675eb54fa8ecd5f4b2a040f
Lalit Maganti <lalitmaganti@gmail.com>
2012-12-01 13:06:31 -0700

    blue: Set BOARD_HAVE_OLD_ION_API
    Change-Id: I0e64fb06bd71916d757a31a47bbc6c5e3b736235

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -67,6 +67,9 @@ TARGET_USES_SF_BYPASS := true
 TARGET_USES_C2D_COMPOSITION := true
 BOARD_EGL_CFG := device/sony/blue-common/config/egl.cfg
 
+# We still have the old ION API
+BOARD_HAVE_OLD_ION_API := true
+
 TARGET_PROVIDES_LIBLIGHT := true
 TARGET_PROVIDES_POWERHAL := true
 
