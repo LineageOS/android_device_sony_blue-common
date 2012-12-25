082671d967a56f040fd383fe63cbd316a8784279
codeworkx <codeworkx@cyanogenmod.org>
2012-12-25 19:02:53 +0100

    blue: fix permission on /data/media during update, cleanup
    Change-Id: Iac6bb9ab31a74e0e2552ff2bc6ff3b81f4579e59

--- blue.mk
+++ blue.mk
@@ -3,7 +3,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_eu_supl.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/sony/blue-common/overlay
+COMMON_PATH := device/sony/blue-common
+
+DEVICE_PACKAGE_OVERLAYS += $(COMMON_PATH)/overlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
@@ -25,40 +27,61 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
     packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
 
-# Bootsplash
+# Ramdisk
 PRODUCT_COPY_FILES += \
-   device/sony/blue-common/prebuilt/logo_X.rle:root/logo.rle
+    $(COMMON_PATH)/prebuilt/mr:root/sbin/mr \
+    $(COMMON_PATH)/prebuilt/logo_X.rle:root/logo.rle \
+    $(COMMON_PATH)/config/fstab.sony:root/fstab.sony \
+    $(COMMON_PATH)/config/fstab_sd.sony:root/fstab_sd.sony \
+    $(COMMON_PATH)/config/init.netconfig.sh:system/etc/init.netconfig.sh \
+    $(COMMON_PATH)/config/init.qcom-etc.rc:root/init.qcom-etc.rc \
+    $(COMMON_PATH)/config/init.qcom.bt.sh:system/etc/init.qcom.bt.sh \
+    $(COMMON_PATH)/config/init.qcom.efs.sync.sh:system/etc/init.qcom.efs.sync.sh \
+    $(COMMON_PATH)/config/init.qcom.class_core.sh:root/init.qcom.class_core.sh \
+    $(COMMON_PATH)/config/init.qcom.class_main.sh:root/init.qcom.class_main.sh \
+    $(COMMON_PATH)/config/init.qcom.coex.sh:system/etc/init.qcom.coex.sh \
+    $(COMMON_PATH)/config/init.qcom.fm.sh:system/etc/init.qcom.fm.sh \
+    $(COMMON_PATH)/config/init.qcom.modem_links.sh:system/etc/init.qcom.modem_links.sh \
+    $(COMMON_PATH)/config/init.qcom.post_boot.sh:system/etc/init.qcom.post_boot.sh \
+    $(COMMON_PATH)/config/init.qcom.usb.rc:root/init.qcom.usb.rc \
+    $(COMMON_PATH)/config/init.qcom.usb.sh:root/init.qcom.usb.sh \
+    $(COMMON_PATH)/config/init.sony.rc:root/init.sony.rc \
+    $(COMMON_PATH)/config/ueventd.sony.rc:root/ueventd.sony.rc
+
+# Script for fixing perms on internal sdcard
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/prebuilt/fix_storage_permissions.sh:system/bin/fix_storage_permissions.sh
+
+# Post recovery script
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/recovery/postrecoveryboot.sh:recovery/root/sbin/postrecoveryboot.sh
+
+# CNE config
+PRODUCT_COPY_FILES += \
+   $(COMMON_PATH)/config/OperatorPolicy.xml:system/etc/OperatorPolicy.xml \
+   $(COMMON_PATH)/config/UserPolicy.xml:system/etc/UserPolicy.xml
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/config/audio_policy.conf:system/etc/audio_policy.conf \
+    $(COMMON_PATH)/config/media_codecs.xml:system/etc/media_codecs.xml \
+    $(COMMON_PATH)/config/snd_soc_msm_2x:system/etc/snd_soc_msm/snd_soc_msm_2x
+
+# Thermal monitor configuration
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/config/thermald.conf:system/etc/thermald.conf
 
 # GPS
 PRODUCT_COPY_FILES += \
-   device/sony/blue-common/prebuilt/gps.conf:system/etc/gps.conf
+    $(COMMON_PATH)/prebuilt/gps.conf:system/etc/gps.conf
 
 # EGL config
 PRODUCT_COPY_FILES += \
-    device/sony/blue-common/config/egl.cfg:system/lib/egl/egl.cfg
+    $(COMMON_PATH)/config/egl.cfg:system/lib/egl/egl.cfg
 
 # WPA supplicant config
 PRODUCT_COPY_FILES += \
-    device/sony/blue-common/config/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
-
-# Common Qualcomm scripts
-PRODUCT_COPY_FILES += \
-    device/sony/blue-common/config/init.qcom.efs.sync.sh:system/etc/init.qcom.efs.sync.sh
-
-PRODUCT_COPY_FILES += \
-   device/sony/blue-common/config/fstab.sony:root/fstab.sony \
-   device/sony/blue-common/config/fstab_sd.sony:root/fstab_sd.sony
-
-# QCOM Display
-PRODUCT_PACKAGES += \
-    copybit.msm8960 \
-    gralloc.msm8960 \
-    hwcomposer.msm8960 \
-    libgenlock \
-    libmemalloc \
-    liboverlay \
-    libqdutils \
-    libtilerenderer
+    $(COMMON_PATH)/config/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf  
 
 # NFC Support
 PRODUCT_PACKAGES += \
@@ -73,12 +96,23 @@ PRODUCT_COPY_FILES += \
 
 # NFCEE access control
 ifeq ($(TARGET_BUILD_VARIANT),user)
-    NFCEE_ACCESS_PATH := device/sony/blue-common/config/nfcee_access.xml
+    NFCEE_ACCESS_PATH := $(COMMON_PATH)/config/nfcee_access.xml
 else
-    NFCEE_ACCESS_PATH := device/sony/blue-common/config/nfcee_access_debug.xml
+    NFCEE_ACCESS_PATH := $(COMMON_PATH)/config/nfcee_access_debug.xml
 endif
 PRODUCT_COPY_FILES += \
     $(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml
+
+# QCOM Display
+PRODUCT_PACKAGES += \
+    copybit.msm8960 \
+    gralloc.msm8960 \
+    hwcomposer.msm8960 \
+    libgenlock \
+    libmemalloc \
+    liboverlay \
+    libqdutils \
+    libtilerenderer
 
 # Audio
 PRODUCT_PACKAGES += \
@@ -87,10 +121,6 @@ PRODUCT_PACKAGES += \
     audio.primary.msm8960 \
     libaudioutils \
     tinymix
-
-PRODUCT_COPY_FILES += \
-    device/sony/blue-common/config/audio_policy.conf:system/etc/audio_policy.conf \
-    device/sony/blue-common/config/media_codecs.xml:system/etc/media_codecs.xml
 
 # Omx
 PRODUCT_PACKAGES += \
@@ -148,43 +178,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_TAGS += dalvik.gc.type-precise
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-
-# Custom init / uevent
-PRODUCT_COPY_FILES += \
-    device/sony/blue-common/config/init.netconfig.sh:system/etc/init.netconfig.sh \
-    device/sony/blue-common/config/init.qcom-etc.rc:root/init.qcom-etc.rc \
-    device/sony/blue-common/config/init.qcom.bt.sh:system/etc/init.qcom.bt.sh \
-    device/sony/blue-common/config/init.qcom.class_core.sh:root/init.qcom.class_core.sh \
-    device/sony/blue-common/config/init.qcom.class_main.sh:root/init.qcom.class_main.sh \
-    device/sony/blue-common/config/init.qcom.coex.sh:system/etc/init.qcom.coex.sh \
-    device/sony/blue-common/config/init.qcom.fm.sh:system/etc/init.qcom.fm.sh \
-    device/sony/blue-common/config/init.qcom.modem_links.sh:system/etc/init.qcom.modem_links.sh \
-    device/sony/blue-common/config/init.qcom.post_boot.sh:system/etc/init.qcom.post_boot.sh \
-    device/sony/blue-common/config/init.qcom.usb.rc:root/init.qcom.usb.rc \
-    device/sony/blue-common/config/init.qcom.usb.sh:root/init.qcom.usb.sh \
-    device/sony/blue-common/config/init.sony.rc:root/init.sony.rc \
-    device/sony/blue-common/config/ueventd.sony.rc:root/ueventd.sony.rc
-
-# Post recovery script
-PRODUCT_COPY_FILES += \
-    device/sony/blue-common/recovery/postrecoveryboot.sh:recovery/root/sbin/postrecoveryboot.sh
-
-# Additional sbin stuff
-PRODUCT_COPY_FILES += \
-    device/sony/blue-common/prebuilt/mr:root/sbin/mr
-
-# CNE config
-PRODUCT_COPY_FILES += \
-   device/sony/blue-common/config/OperatorPolicy.xml:system/etc/OperatorPolicy.xml \
-   device/sony/blue-common/config/UserPolicy.xml:system/etc/UserPolicy.xml
-
-# ALSA configuration
-PRODUCT_COPY_FILES += \
-    device/sony/blue-common/config/snd_soc_msm_2x:system/etc/snd_soc_msm/snd_soc_msm_2x
-
-# Thermal monitor configuration
-PRODUCT_COPY_FILES += \
-    device/sony/blue-common/config/thermald.conf:system/etc/thermald.conf
 
 # Set default USB interface
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
