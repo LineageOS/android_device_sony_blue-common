897e22a8e916f89ebc0ce2861cebcbebebafeded
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-02-19 12:01:15 +0100

    blue: board update
    * switch to caf based display and audio
    * enable krait/cortex a9 optimization
    * remove duplicate egl configuration
    * add qseecomd and qcamerasvr services
    * remove SonyQualcomm8x60RIL custom RIL
    * update kernel command line param
    * update ueventd
    
    Change-Id: Iaf2ef969914a4438035fa45c67dd54414081452f
    
    Conflicts:
    	BoardConfigCommon.mk
    	blue.mk
    	include/linux/msm_audio.h
    
    manual rebase of http://review.cyanogenmod.org/#/c/32173/

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+TARGET_SPECIFIC_HEADER_PATH := device/sony/blue-common/include
+
 # inherit from Sony common
 -include device/sony/common/BoardConfigCommon.mk
 
@@ -26,12 +28,19 @@ TARGET_KERNEL_SOURCE := kernel/sony/msm8x60
 # Platform
 TARGET_BOOTLOADER_BOARD_NAME := MSM8960
 TARGET_BOARD_PLATFORM := msm8960
+BOARD_VENDOR := sony
 BOARD_VENDOR_PLATFORM := blue
 
 # Architecture
+TARGET_ARCH := arm
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_ARCH_VARIANT_CPU := cortex-a9
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
 
-# Architecture
+# Flags
 TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
@@ -47,23 +56,33 @@ TARGET_KRAIT_BIONIC_PLDSIZE   := 64
 # Kernel information
 BOARD_KERNEL_CMDLINE := # This is ignored by sony's bootloader
 BOARD_KERNEL_BASE := 0x80200000
-BOARD_RECOVERY_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
 SONY_FORCE_RAMDISK_ADDRESS := 0x81700000
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01500000
 
 # Wifi
-BOARD_HAS_QCOM_WLAN              := true
-BOARD_WLAN_DEVICE                := qcwcn
-WPA_SUPPLICANT_VERSION           := VER_0_8_X
-BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-BOARD_HOSTAPD_DRIVER             := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wlan.ko"
-WIFI_DRIVER_MODULE_NAME          := "wlan"
-WIFI_DRIVER_FW_PATH_STA          := "sta"
-WIFI_DRIVER_FW_PATH_AP           := "ap"
+BOARD_HAS_QCOM_WLAN                 := true
+BOARD_WLAN_DEVICE                   := qcwcn
+WPA_SUPPLICANT_VERSION              := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER         := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB    := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_HOSTAPD_DRIVER                := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB           := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+WIFI_DRIVER_MODULE_PATH             := "/system/lib/modules/wlan.ko"
+WIFI_DRIVER_MODULE_NAME             := "wlan"
+WIFI_DRIVER_FW_PATH_STA             := "sta"
+WIFI_DRIVER_FW_PATH_AP              := "ap"
+
+# QCOM hardware
+BOARD_USES_QCOM_HARDWARE := true
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
+
+# Graphics
+TARGET_QCOM_DISPLAY_VARIANT := caf
+USE_OPENGL_RENDERER := true
+TARGET_USES_ION := true
+TARGET_USES_C2D_COMPOSITION := true
+BOARD_EGL_CFG := device/sony/blue-common/config/egl.cfg
 
 TARGET_PROVIDES_LIBLIGHT := true
 
@@ -79,6 +98,9 @@ BLUETOOTH_HCI_USE_MCT := true
 # Vold
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
 
+# Vold
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
+
 # Custom boot
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_RECOVERY_PRE_COMMAND := "touch /cache/recovery/boot;sync;"
@@ -89,6 +111,7 @@ BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/sony/blue-common/recovery/recov
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
 
 # Audio
+TARGET_QCOM_AUDIO_VARIANT := caf
 BOARD_USES_ALSA_AUDIO := true
 TARGET_USES_QCOM_MM_AUDIO := true
 BOARD_USES_FLUENCE_INCALL := true
@@ -108,6 +131,9 @@ SOMC_CFG_SENSORS_GYRO_MPU3050 = yes
 SOMC_CFG_SENSORS_PROXIMITY_APDS9702 = yes
 SOMC_CFG_SENSORS_ACCEL_BMA250NA_INPUT = yes
 SOMC_CFG_SENSORS_COMPASS_AK8972 = yes
+
+# Kernel time optimization
+KERNEL_HAS_GETTIMEOFDAY_HELPER := true
 
 # inherit from the proprietary version
 -include vendor/sony/blue-common/BoardConfigVendor.mk
