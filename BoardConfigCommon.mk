5849b965230e02b0457f43a0045510b5cb7ab6b3
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-03-02 21:16:15 +0100

    blue: remove qcom-common stuff
    also switch to Sony 9.1.A.0.489 kernel:
    
    * switch to caf based display and audio
    * enable krait/cortex a9 optimization
    * remove duplicate egl configuration
    * add qseecomd and qcamerasvr services
    * remove SonyQualcomm8x60RIL custom RIL
    * update kernel command line param
    * update ueventd
    * add header from 489 kernel
    * update audio policy
    
    Change-Id: Ibc3c5e31a6e3e71522fc5a911c6a4f42a95ba6b8

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2012 The CyanogenMod Project
+# Copyright (C) 2013 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,28 +12,37 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#TARGET_SPECIFIC_HEADER_PATH := device/sony/blue-common/include
+# inherit from Sony common
+-include device/sony/common/BoardConfigCommon.mk
 
-TARGET_NO_BOOTLOADER := true
-TARGET_NO_RADIOIMAGE := true
-BOARD_HAS_NO_MISC_PARTITION := true
+# inherit from qcom-common
+-include device/sony/qcom-common/BoardConfigCommon.mk
+
+TARGET_SPECIFIC_HEADER_PATH := device/sony/blue-common/include
 
 # Kernel properties
 TARGET_KERNEL_SOURCE := kernel/sony/msm8x60
 
 # Platform
-TARGET_BOOTLOADER_BOARD_NAME := blue
+TARGET_BOOTLOADER_BOARD_NAME := MSM8960
 TARGET_BOARD_PLATFORM := msm8960
+BOARD_VENDOR_PLATFORM := blue
 
 # Architecture
-TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_SMP := true
-TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
-ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_ARCH_VARIANT_CPU := cortex-a9
+
+# Architecture
+TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
+
+# Krait optimizations
+TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
+TARGET_USE_KRAIT_PLD_SET      := true
+TARGET_KRAIT_BIONIC_PLDOFFS   := 10
+TARGET_KRAIT_BIONIC_PLDTHRESH := 10
+TARGET_KRAIT_BIONIC_BBTHRESH  := 64
+TARGET_KRAIT_BIONIC_PLDSIZE   := 64
 
 # Kernel information
 BOARD_KERNEL_CMDLINE := # This is ignored by sony's bootloader
@@ -44,25 +53,17 @@ SONY_FORCE_RAMDISK_ADDRESS := 0x81700000
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01500000
 
 # Wifi
-BOARD_HAS_QCOM_WLAN                 := true
-BOARD_WLAN_DEVICE                   := qcwcn
-WPA_SUPPLICANT_VERSION              := VER_0_8_X
-BOARD_WPA_SUPPLICANT_DRIVER         := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB    := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-BOARD_HOSTAPD_DRIVER                := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB           := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-WIFI_DRIVER_MODULE_PATH             := "/system/lib/modules/wlan.ko"
-WIFI_DRIVER_MODULE_NAME             := "wlan"
-WIFI_DRIVER_FW_PATH_STA             := "sta"
-WIFI_DRIVER_FW_PATH_AP              := "ap"
-
-# Graphics
-USE_OPENGL_RENDERER := true
-TARGET_USES_ION := true
-TARGET_USES_OVERLAY := true
-TARGET_USES_SF_BYPASS := true
-TARGET_USES_C2D_COMPOSITION := true
-BOARD_EGL_CFG := device/sony/blue-common/config/egl.cfg
+BOARD_HAS_QCOM_WLAN              := true
+BOARD_WLAN_DEVICE                := qcwcn
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wlan.ko"
+WIFI_DRIVER_MODULE_NAME          := "wlan"
+WIFI_DRIVER_FW_PATH_STA          := "sta"
+WIFI_DRIVER_FW_PATH_AP           := "ap"
 
 TARGET_PROVIDES_LIBLIGHT := true
 
@@ -75,13 +76,8 @@ BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_QCOM := true
 BLUETOOTH_HCI_USE_MCT := true
 
-# FM Radio
-BOARD_USES_STE_FMRADIO := true
-COMMON_GLOBAL_CFLAGS += -DSTE_FM
-
-# Webkit
-ENABLE_WEBGL := true
-TARGET_FORCE_CPU_UPLOAD := true
+# Vold
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
 
 # Custom boot
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
@@ -94,9 +90,16 @@ BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
 
 # Audio
 BOARD_USES_ALSA_AUDIO := true
+TARGET_USES_QCOM_MM_AUDIO := true
 BOARD_USES_FLUENCE_INCALL := true
 BOARD_USES_SEPERATED_AUDIO_INPUT := true
-BOARD_AUDIO_EXPECTS_MIN_BUFFERSIZE := true
+
+# FM radio
+BOARD_USES_STE_FMRADIO := true
+COMMON_GLOBAL_CFLAGS += -DSTE_FM
+
+# Kernel time optimization
+KERNEL_HAS_GETTIMEOFDAY_HELPER := true
 
 # Sensors
 SOMC_CFG_SENSORS := true
@@ -105,7 +108,6 @@ SOMC_CFG_SENSORS_GYRO_MPU3050 = yes
 SOMC_CFG_SENSORS_PROXIMITY_APDS9702 = yes
 SOMC_CFG_SENSORS_ACCEL_BMA250NA_INPUT = yes
 SOMC_CFG_SENSORS_COMPASS_AK8972 = yes
-
 
 # inherit from the proprietary version
 -include vendor/sony/blue-common/BoardConfigVendor.mk
