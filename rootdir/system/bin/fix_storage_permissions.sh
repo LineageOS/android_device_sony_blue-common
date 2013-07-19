4c2a38ce93782a788309bfde60c9977c5cba0b33
Chirayu Desai <cdesai@cyanogenmod.org>
2013-07-19 21:02:00 +0530

    Initial SELinux policy
    Change-Id: I7ed665d9dc51c6c486453396da2d56f3f3e7256f

--- rootdir/system/bin/fix_storage_permissions.sh
+++ rootdir/system/bin/fix_storage_permissions.sh
@@ -1,3 +1,7 @@
 #!/sbin/sh
 # Fix permissions on internal sdcard
 find /sdcard -user 1023 -exec chown 2800:2800 {} \;
+# Fix SELinux context labelling on internal sdcard
+# Not sure if sdcard_internal/external, and don't even tell me that this
+# isn't the best way to do it - it's not, but currently there isn't any cleaner way.
+find /sdcard -user 2800 -exec toolbox chcon u:object_r:sdcard_external:s0 {} \;
