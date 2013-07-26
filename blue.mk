e0ca4a476bddb180d0b65cb14423958083c6458c
Lalit Maganti <lalitmaganti@gmail.com>
2013-07-26 19:10:15 +0200

    blue: squashed commit of required things for building
    * remove bugmail
    * add TARGET VARIANT
    * temporarily disable FM radio until stabilization
    
    Change-Id: I04d6f9387180d363e554465338de844d1d3680f2

--- blue.mk
+++ blue.mk
@@ -222,11 +222,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.call_ring.multiple=0
 
-# for bugmailer
-PRODUCT_PACKAGES += send_bug
-PRODUCT_COPY_FILES += \
-    system/extras/bugmailer/bugmailer.sh:system/bin/bugmailer.sh \
-    system/extras/bugmailer/send_bug:system/bin/send_bug
-
 # Include non-opensource parts
 $(call inherit-product, vendor/sony/blue-common/blue-common-vendor.mk)
