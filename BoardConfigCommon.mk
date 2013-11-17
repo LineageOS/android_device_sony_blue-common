45259fee306ab05652816013f7a718b2366a1dde
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-11-17 20:28:43 +0100

    blue: Enable NEEDS_VECTORIMPL_SYMBOLS required for the blobs.
    Change-Id: I685759a671449b20b5edef75eef43a344780faca

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -83,6 +83,9 @@ TARGET_NO_RPC := true
 BOARD_HAVE_BLUETOOTH_QCOM := true
 BLUETOOTH_HCI_USE_MCT := true
 
+# Needed for blobs
+COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
+
 # Vold
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
 
