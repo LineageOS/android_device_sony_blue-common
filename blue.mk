a268a9cf45b6746be46438bb7efe50ac4540b9e3
Lalit Maganti <lalitmaganti@gmail.com>
2014-03-26 21:41:19 +0000

    blue: split init.qcom.rc into two files
    * make one file have only qcom stuff and the other only sony stuff
    
    Change-Id: I6147c3f57ca2356308e1d28675edc5fc65dcf0ae

--- blue.mk
+++ blue.mk
@@ -43,6 +43,7 @@ PRODUCT_COPY_FILES += \
 # Ramdisk
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/init.qcom.rc:root/init.qcom.rc \
+    $(COMMON_PATH)/rootdir/init.sony.rc:root/init.sony.rc \
     $(COMMON_PATH)/rootdir/fstab.qcom:root/fstab.qcom \
     $(COMMON_PATH)/rootdir/fstab.qcom:recovery/root/fstab.qcom \
     $(COMMON_PATH)/rootdir/init.recovery.qcom.rc:root/init.recovery.qcom.rc \
