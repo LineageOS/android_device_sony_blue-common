a604ca5c797f78893c145f2f852e86d281ca6d9a
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2015-01-06 13:15:42 +0100

    blue-common: audio: update policy conf, remove redundant qcom flags
    Change-Id: Ib5ba330e5d944e9fb48d673296453f7589fc1ae7

--- blue.mk
+++ blue.mk
@@ -138,7 +138,6 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     alsa.msm8960 \
-    audio_policy.msm8960 \
     audio.primary.msm8960 \
     audio.a2dp.default \
     audio.usb.default \
@@ -197,7 +196,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.audio.fluence.mode=endfire \
     persist.audio.handset.mic=analog \
     persist.audio.lowlatency.rec=false \
-    af.resampler.quality=255 \
     ro.qc.sdk.audio.fluencetype=none \
     lpa.decode=true
 
