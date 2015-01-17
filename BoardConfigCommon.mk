947b4fe159bf9e8c8e2da569f44e853681f566dd
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2015-01-17 19:43:31 +0100

    blue-common: sepolicy: add property contexts
    Change-Id: Id4c7f406cc5505042464a1f14015f322b57ee36b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -144,12 +144,14 @@ BOARD_SEPOLICY_DIRS += \
 
 BOARD_SEPOLICY_UNION += \
     file_contexts \
+    property_contexts \
     bootanim.te \
     illumination.te \
     init.te \
     mac_update.te \
     mediaserver.te \
     platform_app.te \
+    property.te \
     rmt_storage.te \
     secchand.te \
     setup_fs.te \
