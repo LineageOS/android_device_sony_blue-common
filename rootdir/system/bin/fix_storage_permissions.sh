d13a54b7ec4d4f45a5733db943e4b6dd293607e4
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2015-02-26 17:23:00 +0100

    blue: fix the fix
    Change-Id: I51f168051551532d9d9850a81421edb396c16ff6

--- rootdir/system/bin/fix_storage_permissions.sh
+++ rootdir/system/bin/fix_storage_permissions.sh
@@ -1,4 +1,5 @@
 #!/sbin/sh
 
 # Fix permissions on internal sdcard
-find /sdcard -user 2800 -exec chown 1023:1023 {} \;
+chown root:1023 /storage/sdcard0
+find /storage/sdcard0 -user 2800 -exec chown 1023:1023 {} \;
