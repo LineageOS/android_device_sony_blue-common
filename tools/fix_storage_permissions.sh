8255291d641f80522d1b0d157a85ed7979accfcc
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2015-02-27 19:47:03 +0100

    blue: move the fix to install/bin
    Change-Id: I19651f64235219cc96c4b88578ec1b153f6b2b54

--- tools/fix_storage_permissions.sh
+++ tools/fix_storage_permissions.sh
@@ -1,5 +1,11 @@
 #!/sbin/sh
 
+# Mount internal sdcard
+mount -t auto /dev/block/mmcblk0p15 /storage/sdcard0
+
 # Fix permissions on internal sdcard
 chown root:1023 /storage/sdcard0
 find /storage/sdcard0 -user 2800 -exec chown 1023:1023 {} \;
+
+# Unmount internal sdcard
+umount -l /storage/sdcard0
