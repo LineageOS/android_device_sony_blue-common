f917a2029dc8c52e1cac1d6cbf8b63ed213337f3
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2015-02-27 21:45:03 +0100

    blue: perms fix: mount/unmount sdcard if needed
    Change-Id: I1d9814a9567768855eeab746b632654f911430bb

--- tools/fix_storage_permissions.sh
+++ tools/fix_storage_permissions.sh
@@ -1,11 +1,20 @@
 #!/sbin/sh
 
-# Mount internal sdcard
-mount -t auto /dev/block/mmcblk0p15 /storage/sdcard0
+# Create mountpoint for internal sdcard
+mkdir -p /storage/sdcard0
+
+# Mount internal sdcard if needed
+if ! mount | grep "on /storage/sdcard0 type" > /dev/null
+then
+    mount -t auto /dev/block/mmcblk0p15 /storage/sdcard0
+fi
 
 # Fix permissions on internal sdcard
 chown root:1023 /storage/sdcard0
 find /storage/sdcard0 -user 2800 -exec chown 1023:1023 {} \;
 
-# Unmount internal sdcard
-umount -l /storage/sdcard0
+# Unmount internal sdcard if needed
+if mount | grep "on /storage/sdcard0 type" > /dev/null
+then
+    umount -l /storage/sdcard0
+fi
