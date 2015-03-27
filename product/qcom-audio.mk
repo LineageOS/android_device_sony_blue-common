cfa3f2df08b33c84eea17b9373d188f789f6c3b4
Mike Lockwood <lockwood@google.com>
2015-03-27 13:18:43 -0700

    blue-common: usb: Enable MIDI support
     * Includes commits :
       - Iebfb15fcc2c8b793c8392cbba7991fc669e7f72f
       - Ibcf74af9e7f4537497c4cf35777e0ffd29858847
    
     * Adapted to new USB suffix structure
    
    Change-Id: I64d02681451f461dd0518576962a81d6bc0320f3

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -1,3 +1,7 @@
+# Audio permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml
+
 # Audio packages
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
