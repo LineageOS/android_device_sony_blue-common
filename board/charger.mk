e3dc4e891d2d1370e0e31f51cd978b81f09c18f9
Adrian DC <radian.dc@gmail.com>
2015-11-24 20:23:39 +0100

    blue-common: healthd: Update offline charger graphics and library
     * Use a custom resources created out of the original SONY charger
     * Allow Android to properly boot directly from the offline charger
    
     * Implement the libhealthd.msm static library
    
     * Includes :
         Icf01407e57ba94274d3ca558fbf71be2864601d0
         I3bba3b58daba6cffa4eb1314a6da1542f43e8e47
         I6c3bf6abbb4e11dc010c360ecfe9f3bb3e2f3548
         Ibf2ee853657c602158af219de0ba132f29dc4571
    
    Change-Id: I105b7b5ee7eaffac8aeea491a0f7fcfbd736947f
    Signed-off-by: AdrianDC <radian.dc@gmail.com>

---
+++ board/charger.mk
@@ -0,0 +1,8 @@
+# Healthd
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGER_DISABLE_INIT_BLANK := true
+BOARD_HAL_STATIC_LIBRARIES := libhealthd.msm
+BOARD_HEALTHD_CUSTOM_CHARGER_RES := $(COMMON_PATH)/charger/images
+RED_LED_PATH := /sys/class/leds/pwr-red/brightness
+GREEN_LED_PATH := /sys/class/leds/pwr-green/brightness
+BLUE_LED_PATH := /sys/class/leds/pwr-blue/brightness
