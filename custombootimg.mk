f799c0e13e0e1cc6a25b451dac385ab98cee4bd7
AdrianDC <radian.dc@gmail.com>
2016-03-07 20:09:13 +0100

    blue-common: Rewrite the custombootimg.mk for bootimage and recoveryimage
    huashan: Rewrite the custombootimg.mk for bootimage and recoveryimage
    
     * Properly build the recovery.img in a fully clean build tree
     * Handle a local ramdisk-recovery.cpio generation to include the logo.rle,
        the new rule is named _device to avoid using the common rule
    
    Change-Id: I8d888900395666732205aa02fb4e286f9602eccc
    Signed-off-by: AdrianDC <radian.dc@gmail.com>
    
    huashan: Add assert-max-image-size to boot and recovery rules
     * Thanks to Matt Mower for pointing out the absence of
        this partition size check in all sources since the beginning
    
    Change-Id: I22dc3f100679e6c58af6e7f9952ea9040250771b
    
    huashan: Fix the combined bootimage recovery build
     * At the time the bootimage was built on Nightly releases,
        the recoveryimage had not been built yet, and the internal
        recovery files were not all available for the final image,
        resulting in an incomplete in-bootimage recovery ramdisk
    
     * Fix the uncompressed recovery ramdisk generation
        by calling the "build-recoveryramdisk" rule like
        https://github.com/CyanogenMod/android_build/blob/cm-13.0/core/Makefile#L10$
    
    Change-Id: I8b04bfd00629715fabd1ac82ca3df20d5afbab2f
    
    huashan: Isolate device specific parameters of the custombootimg
    
     * Minimize the custombootimg.mk differences between device trees
        from huashan and blue-common to the first parameters lines
    
    Change-Id: I607cd4462d8db7ebdbc4d17727518859cdee372d
    Signed-off-by: AdrianDC <radian.dc@gmail.com>
    
    Change-Id: I6df9bfa61ef8ea3baefad98dee1d57f1b15ca2d3

--- custombootimg.mk
+++ custombootimg.mk
@@ -1,6 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 
 DEVICE_ROOTDIR := device/sony/blue-common/rootdir
+DEVICE_CMDLINE := $(DEVICE_ROOTDIR)/cmdline.txt
+DEVICE_LOGORLE := $(DEVICE_ROOTDIR)/logo.rle
+DEVICE_RPMBIN := vendor/sony/blue-common/proprietary/boot/RPM.bin
 INITSONY := $(PRODUCT_OUT)/utilities/init_sony
 MKELF := device/sony/blue-common/tools/mkelf.py
 
@@ -8,14 +11,45 @@ uncompressed_ramdisk := $(PRODUCT_OUT)/ramdisk.cpio
 $(uncompressed_ramdisk): $(INSTALLED_RAMDISK_TARGET)
 	zcat $< > $@
 
-INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
-$(INSTALLED_BOOTIMAGE_TARGET): $(PRODUCT_OUT)/kernel $(uncompressed_ramdisk) $(recovery_uncompressed_ramdisk) $(INSTALLED_RAMDISK_TARGET) $(INITSONY) $(PRODUCT_OUT)/utilities/toybox $(PRODUCT_OUT)/utilities/keycheck $(MKBOOTIMG) $(MINIGZIP) $(INTERNAL_BOOTIMAGE_FILES)
-	$(call pretty,"Boot image: $@")
+recovery_uncompressed_ramdisk := $(PRODUCT_OUT)/ramdisk-recovery.cpio
+recovery_uncompressed_device_ramdisk := $(PRODUCT_OUT)/ramdisk-recovery-device.cpio
+$(recovery_uncompressed_device_ramdisk): $(MKBOOTFS) \
+		$(INTERNAL_RECOVERYIMAGE_FILES) \
+		$(recovery_initrc) $(recovery_sepolicy) $(recovery_kernel) \
+		$(INSTALLED_2NDBOOTLOADER_TARGET) \
+		$(recovery_build_prop) $(recovery_resource_deps) $(recovery_root_deps) \
+		$(recovery_fstab) \
+		$(RECOVERY_INSTALL_OTA_KEYS) \
+		$(INTERNAL_BOOTIMAGE_FILES)
+	$(call build-recoveryramdisk)
+	@echo -e ${CL_CYN}"----- Making uncompressed recovery ramdisk ------"${CL_RST}
+	$(hide) cp $(DEVICE_LOGORLE) $(TARGET_RECOVERY_ROOT_OUT)/
+	$(hide) $(MKBOOTFS) $(TARGET_RECOVERY_ROOT_OUT) > $@
+	$(hide) rm -f $(recovery_uncompressed_ramdisk)
+	$(hide) cp $(recovery_uncompressed_device_ramdisk) $(recovery_uncompressed_ramdisk)
 
+recovery_ramdisk := $(PRODUCT_OUT)/ramdisk-recovery.img
+$(recovery_ramdisk): $(MINIGZIP) \
+		$(recovery_uncompressed_device_ramdisk)
+	@echo -e ${CL_CYN}"----- Making compressed recovery ramdisk ------"${CL_RST}
+	$(hide) $(MINIGZIP) < $(recovery_uncompressed_ramdisk) > $@
+
+INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
+$(INSTALLED_BOOTIMAGE_TARGET): $(PRODUCT_OUT)/kernel \
+		$(uncompressed_ramdisk) \
+		$(recovery_uncompressed_device_ramdisk) \
+		$(INSTALLED_RAMDISK_TARGET) \
+		$(INITSONY) \
+		$(PRODUCT_OUT)/utilities/toybox \
+		$(PRODUCT_OUT)/utilities/keycheck \
+		$(MKBOOTIMG) $(MINIGZIP) \
+		$(INTERNAL_BOOTIMAGE_FILES)
+
+	@echo -e ${CL_CYN}"----- Making boot image ------"${CL_RST}
 	$(hide) rm -fr $(PRODUCT_OUT)/combinedroot
 	$(hide) mkdir -p $(PRODUCT_OUT)/combinedroot/sbin
 
-	$(hide) cp $(DEVICE_ROOTDIR)/logo.rle $(PRODUCT_OUT)/combinedroot/logo.rle
+	$(hide) cp $(DEVICE_LOGORLE) $(PRODUCT_OUT)/combinedroot/logo.rle
 	$(hide) cp $(uncompressed_ramdisk) $(PRODUCT_OUT)/combinedroot/sbin/
 	$(hide) cp $(recovery_uncompressed_ramdisk) $(PRODUCT_OUT)/combinedroot/sbin/
 	$(hide) cp $(PRODUCT_OUT)/utilities/keycheck $(PRODUCT_OUT)/combinedroot/sbin/
@@ -27,15 +61,17 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(PRODUCT_OUT)/kernel $(uncompressed_ramdisk) $(r
 
 	$(hide) $(MKBOOTFS) $(PRODUCT_OUT)/combinedroot/ > $(PRODUCT_OUT)/combinedroot.cpio
 	$(hide) cat $(PRODUCT_OUT)/combinedroot.cpio | gzip > $(PRODUCT_OUT)/combinedroot.fs
-	$(hide) python $(MKELF) -o $@ $(PRODUCT_OUT)/kernel@0x80208000 $(PRODUCT_OUT)/combinedroot.fs@$(SONY_FORCE_RAMDISK_ADDRESS),ramdisk vendor/sony/blue-common/proprietary/boot/RPM.bin@0x20000,rpm device/sony/blue-common/rootdir/cmdline.txt@cmdline
+	$(hide) python $(MKELF) -o $@ $(PRODUCT_OUT)/kernel@0x80208000 $(PRODUCT_OUT)/combinedroot.fs@0x81900000,ramdisk $(DEVICE_RPMBIN)@0x00020000,rpm $(DEVICE_CMDLINE)@cmdline
 
 	$(hide) ln -f $(INSTALLED_BOOTIMAGE_TARGET) $(PRODUCT_OUT)/boot.elf
+	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE))
+	$(call pretty,"Made boot image: $@")
 
 INSTALLED_RECOVERYIMAGE_TARGET := $(PRODUCT_OUT)/recovery.img
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) \
-	$(recovery_ramdisk) \
-	$(recovery_kernel)
-	@echo ----- Making recovery image ------
-	$(hide) python $(MKELF) -o $@ $(PRODUCT_OUT)/kernel@0x80208000 $(PRODUCT_OUT)/ramdisk-recovery.img@$(SONY_FORCE_RAMDISK_ADDRESS),ramdisk vendor/sony/blue-common/proprietary/boot/RPM.bin@0x20000,rpm device/sony/blue-common/rootdir/cmdline.txt@cmdline
-	@echo ----- Made recovery image -------- $@
-#	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
+		$(recovery_ramdisk) \
+		$(recovery_kernel)
+	@echo -e ${CL_CYN}"----- Making recovery image ------"${CL_RST}
+	$(hide) python $(MKELF) -o $@ $(PRODUCT_OUT)/kernel@0x80208000 $(PRODUCT_OUT)/ramdisk-recovery.img@0x81900000,ramdisk $(DEVICE_RPMBIN)@0x00020000,rpm $(DEVICE_CMDLINE)@cmdline
+	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE))
+	$(call pretty,"Made recovery image: $@")
