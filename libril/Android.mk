e5b2f1abdf122b00051388497a22bb10ff18380c
AdrianDC <radian.dc@gmail.com>
2016-06-11 13:40:19 +0200

    blue-common: Update LibRIL to CM-13.0
    huashan: update libril to 5.1
    From Fusion3
    
    Change-Id: I6fa05ea678ff69255c2a6780a65c0793b2956719
    Signed-off-by: Mrcl1450 <mrcl1450@gmail.com>
    
    huashan: Update LibRIL to CM-13.0
    
     * Implementation of the M hardware-ril
     * Updated SonyRIL class
     * Updated ril.h from hardware-ril
    
     * Include changes from :
        I107f42260a55187816ac08b7972cba175f33c36b
    
    Change-Id: I7e3d46bd0efaaa1e285fa00d87a21b416c1b0745
    Signed-off-by: AdrianDC <radian.dc@gmail.com>
    
    libril: correct include
    
    Change-Id: I9eba8d8f568f19358bd43cf6a3816903d4c3f63d
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>
    
    huashan: Update LibRIL to restore manual handling
    
     * Bring back responseDataRegistrationState and dispatchNetworkManual
     * Cleanup the Android.mk
    
    Change-Id: Iff6175dcdc2a86f876a9431bc57d5d2df3822a8d
    
    Change-Id: I477c8d3bbacd1e7732c6dd7c16ddc8f538497dee
    Signed-off-by: AdrianDC <radian.dc@gmail.com>

--- libril/Android.mk
+++ libril/Android.mk
@@ -7,7 +7,9 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES:= \
     ril.cpp \
-    ril_event.cpp
+    ril_event.cpp \
+    RilSocket.cpp \
+    RilSapSocket.cpp \
 
 LOCAL_SHARED_LIBRARIES := \
     liblog \
@@ -15,56 +17,24 @@ LOCAL_SHARED_LIBRARIES := \
     libbinder \
     libcutils \
     libhardware_legacy \
-    librilutils
+    librilutils \
 
-LOCAL_CFLAGS := -O0
-ifdef BOARD_USES_QCOM_RIL_RESPONSE_5_ELEMENTS
-LOCAL_CFLAGS += -DRIL_RESPONSE_5_ELEMENTS
+LOCAL_STATIC_LIBRARIES := \
+    libprotobuf-c-nano-enable_malloc \
+
+#LOCAL_CFLAGS := -DANDROID_MULTI_SIM -DDSDA_RILD1
+
+ifeq ($(SIM_COUNT), 2)
+    LOCAL_CFLAGS += -DANDROID_SIM_COUNT_2
 endif
 
-ifeq ($(BOARD_RIL_NO_CELLINFOLIST),true)
-LOCAL_CFLAGS += -DRIL_NO_CELL_INFO_LIST
-endif
-
-ifneq ($(BOARD_RIL_NO_SEEK),true)
-LOCAL_CFLAGS += -DRIL_SUPPORTS_SEEK
-endif
-
-ifeq ($(BOARD_HAS_RIL_LEGACY_PAP),true)
-LOCAL_CFLAGS += -DRIL_LEGACY_PAP
-endif
+LOCAL_C_INCLUDES += $(TARGET_OUT_HEADERS)/librilutils
+LOCAL_C_INCLUDES += external/nanopb-c
 
 LOCAL_MODULE:= libril
 
-#USE HCRADIO
-ifeq ($(BOARD_USES_HC_RADIO),true)
-LOCAL_CFLAGS += -DHCRADIO
-endif
-
-ifeq ($(BOARD_USES_LEGACY_RIL),true)
-LOCAL_CFLAGS += -DLEGACY_RIL
-endif
+LOCAL_COPY_HEADERS_TO := libril
+LOCAL_COPY_HEADERS := ril_ex.h
 
 include $(BUILD_SHARED_LIBRARY)
-
-
-# For RdoServD which needs a static library
-# =========================================
-ifneq ($(ANDROID_BIONIC_TRANSITION),)
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES:= \
-    ril.cpp
-
-LOCAL_STATIC_LIBRARIES := \
-    libutils_static \
-    libcutils \
-    librilutils_static
-
-LOCAL_CFLAGS :=
-
-LOCAL_MODULE:= libril_static
-
-include $(BUILD_STATIC_LIBRARY)
-endif # ANDROID_BIONIC_TRANSITION
 endif # BOARD_PROVIDES_LIBRIL
