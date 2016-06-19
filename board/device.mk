859c713720c6872f5634da05c74bcdf92956774c
Adrian DC <radian.dc@gmail.com>
2016-06-19 22:20:30 +0200

    blue-common: Remove Legacy MMAP usage
    Change-Id: I892014a933e969d20d9a10c6c3bd4f06133b2456

--- board/device.mk
+++ board/device.mk
@@ -2,9 +2,6 @@
 BOARD_VENDOR := sony
 BOARD_VENDOR_PLATFORM := blue
 
-# Use legacy MMAP for pre-lollipop blobs
-BOARD_USES_LEGACY_MMAP := true
-
 # Dumpstate
 BOARD_LIB_DUMPSTATE := libdumpstate.sony
 
