405c1e0d597d096220c346c0ea20cf3993b31f08
Adrian DC <radian.dc@gmail.com>
2016-07-06 02:07:10 +0200

    blue-common: Switch to the new msm8960 audio HAL
     * Reupdated thanks to Arne Coucheron :
       * Change-Id I3b180427743f56b384a72f948948a357ae7310d4
       * Deprecate old defines
       * Deprecate old system props and use new ones
       * Change audio_policy.conf for new HAL
       * Remove old sound use case file
    
     * Custom audio_platform_info.xml based on legacy values
     * Implement custom mixer_paths based on legacy hal config
     * Improvements on some audio configurations and volumes
     * Restored support for audio over MHL HDMI
     * Added "Use both 44 and 48KHz for A2DP" by Ron Gokhale
     * Adapted for blue-common from huashan
     * Missing audio property flags added
    
    Change-Id: I88ce0a3ae09619370d92e2deb804c0ff95378fff
    Signed-off-by: AdrianDC <radian.dc@gmail.com>

--- product/audio.mk
+++ product/audio.mk
@@ -1,10 +1,16 @@
 # Audio configurations
 PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(COMMON_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(COMMON_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(COMMON_PATH)/audio/snd_soc_msm_2x:system/etc/snd_soc_msm/snd_soc_msm_2x
+    $(COMMON_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
 # Audio properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.audio.fluence.mode=endfire \
-    persist.audio.handset.mic=analog \
-    persist.audio.lowlatency.rec=false
+    audio.offload.disable=1 \
+    mm.enable.smoothstreaming=true \
+    ro.qc.sdk.audio.fluencetype=none \
+    persist.audio.fluence.voicecall=true \
+    persist.audio.fluence.voicerec=false \
+    persist.audio.fluence.speaker=true \
+    qcom.hw.aac.encoder=true
