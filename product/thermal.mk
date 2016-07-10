f13355dfe8020c086a093bbd83b483137f574655
Adrian DC <radian.dc@gmail.com>
2016-07-10 19:16:52 +0200

    blue-common: thermal: Upgrade to thermanager
     * Add the thermanager repo dependency
     * Compile the package of thermanager
    
     * Add the new thermanager.xml configuration
     * Configuration built upon last huashan state
    
     * Replace sysmon init with thermanager
     * Delete old sysmon proprietary blobs
    
    Change-Id: I0546d424b0628ab6cd29d3320d73d95af522123d
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- product/thermal.mk
+++ product/thermal.mk
@@ -1,3 +1,8 @@
 # Thermal configuration
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/thermald.conf:system/etc/thermald.conf
+    $(COMMON_PATH)/configs/thermald.conf:system/etc/thermald.conf \
+    $(LOCAL_PATH)/configs/thermanager.xml:system/etc/thermanager.xml
+
+# Thermal management packages
+PRODUCT_PACKAGES += \
+    thermanager
