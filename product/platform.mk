3645be1b52c6f805f2f546d8610b6b8332430aa1
Adrian DC <radian.dc@gmail.com>
2016-07-21 22:05:06 +0200

    blue-common: fstab: Migrate to a unified UserData layout
     * Keeping the old 2GB Data & Internal SDCard partitions
        introduces far too much storage space struggles,
        issues for full ext4 support with kernel hacks,
        as well as broken support of ext4 MicroSD cards
        due to a glitch in the blkid / vold combo in this case,
        mixing the UUIDs and the internal / external linkages
    
     * This patch introduces the "normal" emulation of
        internal storage as part of the UserData partition
    
     * A manual deletion of /data/system/storage.xml
        is required in the case someone would decide
        to perform this operation manually
    
     * Next commit will introduce a migration path
        with many safeties to let the user upgrade
        to an unified partition on purpose
    
    Change-Id: I2d5df7793de4f2580f2393495106ca9395a2c23a
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- product/platform.mk
+++ product/platform.mk
@@ -4,10 +4,6 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     ueventd.qcom.rc
 
-# Storage package
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/releasetools/fix_storage_permissions.sh:install/bin/fix_storage_permissions.sh
-
 # Sony TrimArea package
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/sbin/tad_static:root/sbin/tad_static
@@ -19,7 +15,3 @@ PRODUCT_PACKAGES += \
 # Sony MACAddrSetup package
 PRODUCT_PACKAGES += \
     macaddrsetup
-
-# Storage properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vold.primary_physical=1
