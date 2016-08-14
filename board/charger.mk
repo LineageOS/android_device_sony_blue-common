4e644eddf332b0cb3b706b26b804bab5f6f6fcef
Adrian DC <radian.dc@gmail.com>
2016-08-14 11:02:46 +0200

    blue-common: charger: Switch to CM charger libhealthd
     * Define the backlight paths as configurations
     * Remove the custom built Sony charger images
    
     * Remove the device static healthd library, as all
        its features are now handled in healtd_board_cm
    
     * Commit to revert for AOSP-based ROM compilations
    
    Change-Id: I57a4a4d9d98dfe38e39a10e40beca6e468fdd6f5

--- board/charger.mk
+++ board/charger.mk
@@ -1,7 +1,8 @@
 # Healthd
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
-BOARD_HAL_STATIC_LIBRARIES := libhealthd.msm
+BACKLIGHT_PATH := /sys/devices/i2c-10/10-0040/leds/lcd-backlight_1/brightness
+SECONDARY_BACKLIGHT_PATH := /sys/devices/i2c-10/10-0040/leds/lcd-backlight_2/brightness
 BOARD_HEALTHD_CUSTOM_CHARGER_RES := $(COMMON_PATH)/charger/images
 RED_LED_PATH := /sys/class/leds/pwr-red/brightness
 GREEN_LED_PATH := /sys/class/leds/pwr-green/brightness
