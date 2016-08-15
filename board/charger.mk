5c470303a2b591baedf0fa4fc56212ddcaa1d949
Adrian DC <radian.dc@gmail.com>
2016-08-15 16:27:43 +0200

    charger: Optionally use the custom built Sony charger
     * Restore Sony charger images I made in commit
        Icf01407e57ba94274d3ca558fbf71be2864601d0
        "huashan: Implement the healthd offline charger"
    
     * Restore the device static healthd library,
        renamed to libhealthd.board to avoid conflicts
    
     * Includes "charger: Fix healthd header inclusion path"
    
    This reverts commit 292896726b23b7eb5da9257e5eb59538b15d7587.
    Change-Id: I03701d6a4cc86913f901091413f6445d2631aacb
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- board/charger.mk
+++ board/charger.mk
@@ -6,3 +6,9 @@ SECONDARY_BACKLIGHT_PATH := /sys/devices/i2c-10/10-0040/leds/lcd-backlight_2/bri
 RED_LED_PATH := /sys/class/leds/pwr-red/brightness
 GREEN_LED_PATH := /sys/class/leds/pwr-green/brightness
 BLUE_LED_PATH := /sys/class/leds/pwr-blue/brightness
+
+# Healthd library extension
+ifneq ($(BOARD_AOSP_BASED),)
+BOARD_HAL_STATIC_LIBRARIES := libhealthd.board
+BOARD_HEALTHD_CUSTOM_CHARGER_RES := $(DEVICE_PATH)/charger/images
+endif
