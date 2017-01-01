c6d698fb43869e26ed9f32d70365a4e6c5ec7dfa
Adrian DC <radian.dc@gmail.com>
2017-01-01 23:21:06 +0100

    blue-common: proprietary: Use new generic extract utils
     * Based on similar scripts from nicki, jf, ether
     * Add variables handling to ensure clean makefile outputs
    
     * Updated with the removed unreferenced libraries
    
    Change-Id: I09616d6bf9960dadbc564634e1ae1e784c7e88c5
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -18,16 +18,16 @@
 
 set -e
 
+# Abort if device not inherited
+if  [ -z "$DEVICE" ]; then
+    echo "Variable DEVICE not defined, aborting..."
+    exit 1
+fi
+
 # Required!
 export DEVICE_COMMON=blue-common
 export DEVICE_NAME=$DEVICE
 export VENDOR=sony
-
-# Abort if device not inherited
-if  [ -z "$DEVICE_NAME" ]; then
-    echo "Variable DEVICE not defined, aborting..."
-    exit 1
-fi
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
@@ -42,41 +42,37 @@ if [ ! -f "$HELPER" ]; then
 fi
 . "$HELPER"
 
-if [ $# -eq 0 ]; then
-    SRC=adb
-else
-    if [ $# -eq 1 ]; then
-        SRC=$1
-    else
-        echo "$0: bad number of arguments"
-        echo ""
-        echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
-        echo ""
-        echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
-        echo "the device using adb pull."
-        exit 1
-    fi
-fi
-
 # Initialize the helper for common device
 DEVICE=$DEVICE_COMMON
 setup_vendor "$DEVICE" "$VENDOR" "$ROM_ROOT" true
 
+# Copyright headers and common guards
+write_headers "hayabusa mint tsubasa"
+
 # Sony/Board specific blobs
-extract "$MY_DIR"/proprietary-files-sony.txt "$SRC"
+write_makefiles "$MY_DIR"/proprietary-files-sony.txt
+printf '\n' >> "$PRODUCTMK"
 
 # QCom common board blobs
-extract "$MY_DIR"/proprietary-files-qc.txt "$SRC"
+write_makefiles "$MY_DIR"/proprietary-files-qc.txt
 
-# Generate vendor makefiles
-"$MY_DIR"/setup-makefiles.sh
+write_footers
 
 # Reinitialize the helper for device
 DEVICE=$DEVICE_NAME
 setup_vendor "$DEVICE" "$VENDOR" "$ROM_ROOT"
 
+# Copyright headers and guards
+write_headers
+
 # Sony/Device specific blobs
-extract "$MY_DIR"/../$DEVICE/proprietary-files-sony.txt "$SRC"
+write_makefiles "$MY_DIR"/../$DEVICE/proprietary-files-sony.txt
+printf '\n' >> "$PRODUCTMK"
 
 # QCom common device blobs
-extract "$MY_DIR"/../$DEVICE/proprietary-files-qc.txt "$SRC"
+write_makefiles "$MY_DIR"/../$DEVICE/proprietary-files-qc.txt
+
+# Vendor BoardConfig variables
+printf 'USE_CAMERA_STUB := false\n' >> "$BOARDMK"
+
+write_footers
