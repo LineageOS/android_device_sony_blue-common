a1da9770bd9a465d765f0dccb22b756aefa80088
Adrian DC <radian.dc@gmail.com>
2017-01-27 23:39:41 +0100

    blue-common: build: Exclude releasetools install files
    Change-Id: Ic4a7f2195f0bcc7e3d68c5891c427bc850b3faec

--- product/releasetools.mk
+++ product/releasetools.mk
@@ -5,9 +5,9 @@ PRODUCT_PACKAGES += \
     utility_e2fsck
 
 # Release tools scripts
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/releasetools/unify_userdata/edify_tools.sh:install/unify_userdata/edify_tools.sh \
-    $(COMMON_PATH)/releasetools/unify_userdata/main.sh:install/unify_userdata/main.sh \
-    $(COMMON_PATH)/releasetools/unify_userdata/part_tools.sh:install/unify_userdata/part_tools.sh \
-    $(COMMON_PATH)/releasetools/unify_userdata/prop_tools.sh:install/unify_userdata/prop_tools.sh \
-    $(COMMON_PATH)/releasetools/unify_userdata/variables.sh:install/unify_userdata/variables.sh
+# PRODUCT_COPY_FILES += \
+#     $(COMMON_PATH)/releasetools/unify_userdata/edify_tools.sh:install/unify_userdata/edify_tools.sh \
+#     $(COMMON_PATH)/releasetools/unify_userdata/main.sh:install/unify_userdata/main.sh \
+#     $(COMMON_PATH)/releasetools/unify_userdata/part_tools.sh:install/unify_userdata/part_tools.sh \
+#     $(COMMON_PATH)/releasetools/unify_userdata/prop_tools.sh:install/unify_userdata/prop_tools.sh \
+#     $(COMMON_PATH)/releasetools/unify_userdata/variables.sh:install/unify_userdata/variables.sh
