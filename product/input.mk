2c16652462cc3db0557055396660f46c72b866b4
Jaekyun Seok <jaekyun@google.com>
2017-02-28 18:08:32 +0900

    product: Move input device config files into vendor partition
    Change-Id: I848718a8118d88da1f570575c3243bc072a6c6e1

--- product/input.mk
+++ product/input.mk
@@ -1,6 +1,6 @@
 # Keys layouts
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
-    $(COMMON_PATH)/configs/msm8960-snd-card_Button_Jack.kl:system/usr/keylayout/msm8960-snd-card_Button_Jack.kl \
-    $(COMMON_PATH)/configs/pmic8xxx_pwrkey.kl:system/usr/keylayout/pmic8xxx_pwrkey.kl \
-    $(COMMON_PATH)/configs/simple_remote.kl:system/usr/keylayout/simple_remote.kl
+    $(COMMON_PATH)/configs/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl \
+    $(COMMON_PATH)/configs/msm8960-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/msm8960-snd-card_Button_Jack.kl \
+    $(COMMON_PATH)/configs/pmic8xxx_pwrkey.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/pmic8xxx_pwrkey.kl \
+    $(COMMON_PATH)/configs/simple_remote.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/simple_remote.kl
