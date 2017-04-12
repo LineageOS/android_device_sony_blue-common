d2d7a087f3ef2b0281174e3878f9ea695214759d
Adrian DC <radian.dc@gmail.com>
2017-04-12 20:23:25 +0200

    blue-common: product: COMMON_PATH instead of LOCAL_PATH in thermal.mk
     * Same result but cleaner in regard to all other configurations
    
    Change-Id: Iccefde29fa7dfa536b5a87199f5c8ccb33d0fc4b

--- product/thermal.mk
+++ product/thermal.mk
@@ -1,7 +1,7 @@
 # Thermal configuration
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/thermald.conf:system/etc/thermald.conf \
-    $(LOCAL_PATH)/configs/thermanager.xml:system/etc/thermanager.xml
+    $(COMMON_PATH)/configs/thermanager.xml:system/etc/thermanager.xml
 
 # Thermal management packages
 PRODUCT_PACKAGES += \
