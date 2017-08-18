aaeab0fbe1db90cb281ed5ebdd73c98f9a5b769f
Adrian DC <radian.dc@gmail.com>
2017-08-18 22:53:54 +0200

    media: Use AOSP's media_codecs.xml without FFmpeg
     * Use the AOSP media_codecs.xml on LineageOS too
     * Rename media_codecs.xml to ..._ffmpeg.xml
     * Disabled during Oreo bringup
    
    Change-Id: I9951c5e1edf66a6ad7bae015129e9a74087c9091

--- product/media.mk
+++ product/media.mk
@@ -4,7 +4,7 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/media_codecs_aosp.xml:system/etc/media_codecs.xml
 else
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml
+    $(COMMON_PATH)/configs/media_codecs_aosp.xml:system/etc/media_codecs.xml
 endif
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
