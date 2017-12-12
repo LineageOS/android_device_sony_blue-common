bcce9e7aa336d4a362fbbdde9c2a323d44785148
Adrian DC <radian.dc@gmail.com>
2017-12-12 22:11:37 +0100

    media: Revert "Disable Treble OMX by default."
     * Use the Oreo Treble mediacodec service instead
    
    This reverts commit 107eb440a5dd1d1acfa818ba0da2693e579ac098.
    Change-Id: Ia54b742693468a58abc188f78c9173ae2a66ad07

--- product/qcom-media.mk
+++ product/qcom-media.mk
@@ -21,7 +21,3 @@ PRODUCT_PACKAGES += \
 # DRM HIDL interfaces
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl
-
-# OMX properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.media.treble_omx=false
