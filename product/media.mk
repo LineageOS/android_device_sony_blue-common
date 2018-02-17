fb56bac8487152705f53a7dbc3e8052f47d3ad0c
Adrian DC <radian.dc@gmail.com>
2018-02-17 23:20:11 +0100

    media: Fully deprecate media_codecs_ffmpeg.xml
     * Re-unify the file under media_codecs.xml
    
     * Partially removed in commit :
        "media: Use AOSP's media_codecs.xml without FFmpeg"
        Change-Id: I9951c5e1edf66a6ad7bae015129e9a74087c9091
        Date: Fri, 18 Aug 2017 22:53:54 +0200
    
    Change-Id: Ia88336e32fdfef6fb1ce04190f55d15e53d5241c

--- product/media.mk
+++ product/media.mk
@@ -1,11 +1,5 @@
 # Media configurations
-ifneq ($(BOARD_AOSP_BASED),)
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/media_codecs_aosp.xml:system/etc/media_codecs.xml
-else
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/configs/media_codecs_aosp.xml:system/etc/media_codecs.xml
-endif
-PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
     $(COMMON_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
     $(COMMON_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
