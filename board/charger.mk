6dfbb447b92babc4ac19b6fff2f7b01382deb525
Adrian DC <radian.dc@gmail.com>
2018-05-05 18:39:32 +0200

    healthd: Abandon custom healthd in favour to blank support
     * Remove healthd backlight handling
     * Remove unused board configurations
    
    Change-Id: I453fb6c867c519f7dab1cbe5c1fdbd7475aef25b
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- board/charger.mk
+++ board/charger.mk
@@ -1,14 +1,9 @@
 # Healthd
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
-BACKLIGHT_PATH := /sys/devices/i2c-10/10-0040/leds/lcd-backlight_1/brightness
-SECONDARY_BACKLIGHT_PATH := /sys/devices/i2c-10/10-0040/leds/lcd-backlight_2/brightness
 RED_LED_PATH := /sys/class/leds/pwr-red/brightness
 GREEN_LED_PATH := /sys/class/leds/pwr-green/brightness
 BLUE_LED_PATH := /sys/class/leds/pwr-blue/brightness
 
 # Healthd library extension
-ifneq ($(BOARD_AOSP_BASED),)
-BOARD_HAL_STATIC_LIBRARIES := libhealthd.board
-endif
 BOARD_HEALTHD_CUSTOM_CHARGER_RES := $(COMMON_PATH)/charger/images
