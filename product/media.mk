4d7617fb6416b92d495be492c590910695906f71
bohu <bohu@google.com>
2019-05-20 15:22:48 -0700

    media: Disable codec2
    BUG: 133178866
    Change-Id: Ie6698604357adfb7c36d6655ea8a42aecad6b219

--- product/media.mk
+++ product/media.mk
@@ -3,3 +3,7 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(COMMON_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
     $(COMMON_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
+
+# Media properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.stagefright.ccodec=0
