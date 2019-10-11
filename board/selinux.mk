070772a2665a6337b581f4faeb9319a6c9da58a9
Adrian DC <radian.dc@gmail.com>
2019-10-11 21:24:14 +0200

    sepolicy: isolate LineageOS related sepolicies from AOSP builds
    Change-Id: I93ba487fc12cfd2b6944171f13c2706e37eac7cc
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- board/selinux.mk
+++ board/selinux.mk
@@ -1,3 +1,9 @@
 # Common sepolicies
 BOARD_SEPOLICY_DIRS += \
     $(COMMON_PATH)/sepolicy
+
+# LineageOS common sepolicies
+ifeq ($(BOARD_AOSP_BASED),)
+BOARD_SEPOLICY_DIRS += \
+    $(COMMON_PATH)/sepolicy-lineage
+endif
