3650236116184829965ade85c30299ac0ef08641
Adrian DC <radian.dc@gmail.com>
2020-08-14 09:39:30 +0200

    rootdir: deprecate AOSP specific fstab as F2FS has been removed
     * Introduced by change I0557f844d74fdc6aea2e38d24e21e40b76513ccb:
       "fstab: Optionally provide an AOSP compatible fstab configuration"
    
    Change-Id: I4da0266a10c4288e81859ad23e1b7e7431063771

--- board/recovery.mk
+++ board/recovery.mk
@@ -1,7 +1,3 @@
 # Recovery
 RECOVERY_FSTAB_VERSION := 2
-ifneq ($(BOARD_AOSP_BASED),)
-TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/fstab_aosp.qcom
-else
 TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/fstab.qcom
-endif
